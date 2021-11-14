7b50007f927f28aea59eb7019b442420252ee1de
GregorySchwing <gschwing@uno.edu>
2021-11-14 15:19:11 -0600

    iterate through all methods

--- SinglePointEnergy/findOptimumRCutWolfAlpha.py
+++ SinglePointEnergy/findOptimumRCutWolfAlpha.py
@@ -26,31 +26,38 @@ def main(argv):
 	print('Output file is ', outputfile)
 
 	df = pd.read_pickle(inputfile)
-	
-	df2 = df.iloc[:,df.columns.get_level_values(1)==('Vlugt_DSF')]
-	alphas = df.iloc[:,df.columns.get_level_values(1)==('Alpha')]
-	print(alphas)
+	method_pots = []
+	methods = [ "Gross" , "Vlugt", "Cassandra" ]
+	potentials = [ "DSP" , "DSF"]
+	for method in methods:
+		for potential in potentials:
+			colName = "{}_{}".format(method, potential)
+			method_pots.append(colName)
+	for metpot in method_pots:
+		df2 = df.iloc[:,df.columns.get_level_values(1)==(metpot)]
+		alphas = df.iloc[:,df.columns.get_level_values(1)==('Alpha')]
+		print(alphas)
 
-	df2 = pd.MultiIndex.from_frame(df2)
-	print(df2)	
-	print(df2.names)
-	x = []
-	y = []
-	z = []
-	for name in df2.names:
-		for alpha, value in zip(alphas[(name[0], "Alpha")], df2.get_level_values(name)):
-			x.append(alpha) 
-			y.append(name[0]) 
-			z.append(value)
+		df2 = pd.MultiIndex.from_frame(df2)
+		print(df2)	
+		print(df2.names)
+		x = []
+		y = []
+		z = []
+		for name in df2.names:
+			for alpha, value in zip(alphas[(name[0], "Alpha")], df2.get_level_values(name)):
+				x.append(alpha) 
+				y.append(name[0]) 
+				z.append(value)
 
 
-	ax = plt.axes(projection='3d')
-	ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none');
-	ax.set_title('Vlugt DSF', fontsize=20)
-	ax.set_xlabel('Alpha', fontsize=20, labelpad=20)
-	ax.set_ylabel('RCut', fontsize=20, labelpad=20)
-	ax.set_zlabel('Relative Error', fontsize=20, labelpad=20)
-	plt.show()
+		ax = plt.axes(projection='3d')
+		ax.plot_trisurf(x, y, z, cmap='viridis', edgecolor='none');
+		ax.set_title(metpot, fontsize=20)
+		ax.set_xlabel('Alpha', fontsize=20, labelpad=20)
+		ax.set_ylabel('RCut', fontsize=20, labelpad=20)
+		ax.set_zlabel('Relative Error', fontsize=20, labelpad=20)
+		plt.show()
 
 
 if __name__ == "__main__":
