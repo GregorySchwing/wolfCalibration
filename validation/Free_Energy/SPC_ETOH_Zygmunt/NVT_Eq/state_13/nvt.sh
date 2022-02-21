4deb0d5dd2167b2043b8527ab7b5061e2664ef11
Gregory J. Schwing <go2432@wayne.edu>
2022-02-20 23:36:27 -0500

    partial push of fe

---
+++ validation/Free_Energy/SPC_ETOH_Zygmunt/NVT_Eq/state_13/nvt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Job name
+#SBATCH --job-name NVT
+# Submit to the gpu QoS, the requeue QoS can also be used for gpu's 
+#SBATCH -q primary
+# Request one node
+#SBATCH -N 1
+# Total number of cores, in this example it will 1 node with 1 core each.
+#SBATCH -n 8
+# Request memory
+#SBATCH --mem=8G
+# Mail when the job begins, ends, fails, requeues
+#SBATCH --mail-type=ALL
+# Where to send email alerts
+#SBATCH --mail-user=go2432@wayne.edu
+# Create an output file that will be output_<jobid>.out
+#SBATCH -o output_%j.out
+# Create an error file that will be error_<jobid>.out
+#SBATCH -e errors_%j.err
+# Set maximum time limit
+#SBATCH -t 1-0:0:0
+/wsu/home/go/go24/go2432/GOMC/bin/GOMC_CPU_NVT +p8 NVT_Eq_water_ethanol_fe.conf > log
