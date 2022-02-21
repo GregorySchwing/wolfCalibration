18254644efcf6fbe9759d74571ef8af64fcbe109
go2432 <go2432@wayne.edu>
2022-02-21 00:34:29 -0500

    FS

---
+++ validation/Free_Energy/MSPCE_ETOH/Prod_DSP/state_0/testCal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Example with 28 cores for OpenMP
+#
+# Project/Account
+#SBATCH -A greg
+#
+# Number of cores
+#SBATCH -c 6 -w, --nodelist=potoff33
+#
+# Runtime of this jobs is less then 12 hours.
+#SBATCH --time=168:00:00
+#
+#SBATCH --mail-user=go2432@wayne.edu
+
+#SBATCH -o output_%j.out
+
+#SBATCH -e errors_%j.err
+
+
+# Clear the environment from any previously loaded modules
+/home6/greg/GOMC/bin/GOMC_CPU_NPT NPT_Prod_water_ethanol_fe.conf > log 
+# End of submit file
