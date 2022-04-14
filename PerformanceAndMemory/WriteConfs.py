d9dbe8457a1bb8ef9a3470c676c5f2e22f51fb35
GregorySchwing <gschwing@uno.edu>
2022-04-14 00:22:37 -0500

    ready

--- PerformanceAndMemory/WriteConfs.py
+++ PerformanceAndMemory/WriteConfs.py
@@ -72,11 +72,21 @@ print(seed)
 
 calibPathSuffix = "_a"
 
-methods = ["wolf", "ewald"]
+methods = ["Wolf", "Ewald"]
 
+runsdir = Path("runs")
+sysdir = Path("systems")
+runsdir.mkdir(parents=True, exist_ok=True)
+
+box = "0"
+WolfDefaultKind = "Vlugt"
+WolfDefaultPotential = "DSP"
+defaultAlpha = "0.21"
 #for r in range(0, numReplicates, 1):
 
 for boxLength, method in itertools.product(systems, methods):
+
+    path2System ="../../systems/" + str(boxLength) + "_a"
 
     liquid_box_length_Ang = boxLength
     # Build the main simulation liquid box (box 0) [1, 2, 13-17]
@@ -102,7 +112,7 @@ for boxLength, method in itertools.product(systems, methods):
                               reorder_res_in_pdb_psf=True
                               )
 
-    path = Path(str(boxLength) + "_a_" + method)
+    path = runsdir / Path(str(boxLength) + "_a_" + method)
 
     path.mkdir(parents=True, exist_ok=True)
 
@@ -124,6 +134,7 @@ for boxLength, method in itertools.product(systems, methods):
                        "CBMC_Dih" : 50,
                        "Rcut": 14,
                        "RcutLow": 0,
+                       "Ewald": method == "Ewald",
                        "LRC": True,
                        "RcutCoulomb_box_0": 14,
                        "Tolerance" : 0.00005,
@@ -140,13 +151,24 @@ for boxLength, method in itertools.product(systems, methods):
     orig_ff_file_path = Path("toppar_water_ions.str")
     renamed_ff_file_path = Path("NVT_water_FF.inp")
     top = Path("top")
-    sysdir = Path(str(boxLength) + "_a")
 
-    sysdir.mkdir(parents=True, exist_ok=True)
+    boxpath = sysdir / Path(str(boxLength) + "_a")
 
-    shutil.copy((top / orig_ff_file_path), (sysdir / renamed_ff_file_path))  # For Python 3.8+.
+    # make optional
+    boxpath.mkdir(parents=True, exist_ok=True)
 
-    gomc_control.write_gomc_control_file(charmm, NVT_conf_name, 'NVT', RunSteps=NumSteps, Temperature=Temp_in_K, Restart=False, check_input_files_exist=False, ff_psf_pdb_file_directory="../"+ str(boxLength) + "_a", input_variables_dict=input_variables_dict_val)
+    shutil.copy((top / orig_ff_file_path), (boxpath / renamed_ff_file_path))  # For Python 3.8+.
+
+    gomc_control.write_gomc_control_file(charmm, NVT_conf_name, 'NVT', RunSteps=NumSteps, Temperature=Temp_in_K, Restart=False, check_input_files_exist=False, ff_psf_pdb_file_directory=path2System, input_variables_dict=input_variables_dict_val)
+
+    if(method != "Ewald"):
+         with open("NVT_water.conf", "a") as myfile:
+            defPotLine = "Wolf\tTrue\t{pot}\n".format(pot=WolfDefaultPotential)
+            myfile.write(defPotLine)
+            defKindLine = "WolfKind\t{kind}\n".format(kind=WolfDefaultKind)
+            myfile.write(defKindLine)
+            defAlphaLine = "WolfAlpha\t{box}\t{val}\n".format(box=box, val=defaultAlpha)
+            myfile.write(defAlphaLine)
 
     pathConf = Path(NVT_conf_name)
     pathConf.rename(path / pathConf)
@@ -156,20 +178,22 @@ for boxLength, method in itertools.product(systems, methods):
       filedata = file.read()
 
     # Replace the target string
-    filedata = filedata.replace('XXX', str(boxLength))
-
+    if(method == "Ewald"):
+        filedata = filedata.replace('XXX', str(boxLength))
+    else: 
+        filedata = filedata.replace('XXX', str(int(boxLength/10)))
     # Write the file out again
     with open('perfAndMem.sh', 'w') as file:
       file.write(filedata)
 
     bash = Path('perfAndMem.sh')
     bash.rename(path / bash)
-
+    cwd = os.getcwd()
     os.chdir( path )
     #bashCommand = "sbatch perfAndMem.sh"
     bashCommand = "echo $PATH > test"
     process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
     output, error = process.communicate()
-    os.chdir( ".." )
+    os.chdir(cwd)
 
 
