0eb79167bd0e90b77c87ce57bde143b5eac2e48a
GregorySchwing <gschwing@uno.edu>
2022-04-13 23:11:19 -0500

    launch jobs

--- PerformanceAndMemory/perfAndMem.sh
+++ PerformanceAndMemory/perfAndMem.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# Example with 28 cores for OpenMP
-#
 # Project/Account
 #SBATCH --qos=primary
 #
 # Number of cores
-#SBATCH -c 8 -w, 
-#SBATCH --nodelist=wsu205
+#SBATCH -N 1
+# Total number of cores, in this example it will 1 node with 1 core each.
+#SBATCH -n 8
 #SBATCH --mem=1500G
 #
 # Runtime of this jobs is less then 12 hours.
-#SBATCH --time=168:00:00
+#SBATCH --time=24:00:00
 #
 #SBATCH --mail-user=go2432@wayne.edu
 
@@ -19,11 +18,4 @@
 #SBATCH -e errors_%j.err
 
 
-# Clear the environment from any previously loaded modules
-#module purge > /dev/null 2>&1
-#cd /home6/greg/fz/martini/2M/charmm-gui-2758929829/gromacs
-#cd /wsu/home/go/go24/go2432/GOMC
-#./metamake.sh
-#cp ./bin/GOMC_CPU_NVT /wsu/home/go/go24/go2432/big/15m/15mil
-cd /wsu/home/go/go24/go2432/big/15m/15mil
-valgrind --tool=massif ./GOMC_CPU_NVT in.conf 
+valgrind --tool=massif /wsu/home/go/go24/go2432/GOMC/bin/GOMC_CPU_NVT in.conf > log
