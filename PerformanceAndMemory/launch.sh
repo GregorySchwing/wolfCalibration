e662a46050fccf6848724d5680b5d89805194fdb
GregorySchwing <gschwing@uno.edu>
2022-04-21 03:53:02 -0500

    done

--- PerformanceAndMemory/launch.sh
+++ PerformanceAndMemory/launch.sh
@@ -2,11 +2,11 @@
 # Example with 28 cores for OpenMP
 #
 # Project/Account
-#SBATCH --qos=secondary
+#SBATCH -c 1 -w, --nodelist=potoff32
 # Request one node
 #SBATCH -N 1
 # Total number of cores, in this example it will 1 node with 1 core each. 
-#SBATCH -n 4
+#SBATCH -n 1
 #SBATCH --mem=5G
 #
 # Runtime of this jobs is less then 12 hours.
@@ -17,6 +17,6 @@
 #SBATCH -o output_%j.out
 
 #SBATCH -e errors_%j.err
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate mosdef-study38
-python MemoryTest.py
+source /home6/greg/anaconda3/etc
+conda activate mamba
+python3 MemoryTest.py
