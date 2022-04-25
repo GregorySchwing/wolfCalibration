d404c54447e13b098a76b47432bd901c5c04afc8
GregorySchwing <gschwing@uno.edu>
2022-04-25 02:33:55 -0500

    A

--- validation/Free_Energy/prod.sh
+++ validation/Free_Energy/prod.sh
@@ -2,7 +2,7 @@
 # Job name
 #SBATCH --job-name NVT
 # Submit to the gpu QoS, the requeue QoS can also be used for gpu's 
-#SBATCH -q secondary
+#SBATCH -q primary
 # Request one node
 #SBATCH -N 1
 # Total number of cores, in this example it will 1 node with 1 core each.
