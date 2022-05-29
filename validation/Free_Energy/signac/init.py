78b1e376e2a8a634eebda6c3435c3637e7eeb10e
go2432 <go2432@wayne.edu>
2022-05-29 14:43:31 -0500

    1 replica

--- validation/Free_Energy/signac/init.py
+++ validation/Free_Energy/signac/init.py
@@ -14,8 +14,8 @@ project=signac.init_project('nobel_gas_in_water')
 #solute = ["Ne", "Rn"] # ["Ne", "Rn"]
 #solute = ["ETOH"] # ["Ne", "Rn"]
 solute = ["Ne"] # ["Ne", "Rn"]
-replicas = [0, 1, 2, 3, 4] # [0, 1, 2, 3, 4]
-
+#replicas = [0, 1, 2, 3, 4] # [0, 1, 2, 3, 4]
+replicas = [0]# [0, 1, 2, 3, 4]
 #production_temperatures = [275, 295, 315, 335, 355, 375] * u.K # [275, 295, 315, 335, 355, 375] * u.K
 production_temperatures = [275] * u.K # [275, 295, 315, 335, 355, 375] * u.K
 
