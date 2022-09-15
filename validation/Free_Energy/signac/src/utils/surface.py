8e7c9fcff31f3310ee8be0bd8d50b8712cc80f7d
go2432 <go2432@wayne.edu>
2022-09-15 13:03:12 -0500

    dont do quintic

--- validation/Free_Energy/signac/src/utils/surface.py
+++ validation/Free_Energy/signac/src/utils/surface.py
@@ -107,9 +107,9 @@ def find_minimum(path, model, wolfKind, potential, box, plotSuface=False):
     sptdanx_auc = {}
     sptdenx_auc = {}
     #F2 = interpolate.interp2d(x, y, z, kind='linear')
-    #F2 = interpolate.interp2d(x, y, z, kind='cubic')
+    F2 = interpolate.interp2d(x, y, z, kind='cubic')
     # Quintic interpolation performs terribles at the borders (Think 1E6 times too large!)
-    F2 = interpolate.interp2d(x, y, z, kind='quintic')
+    #F2 = interpolate.interp2d(x, y, z, kind='quintic')
 
     X,Y = np.meshgrid(xi,yi)
 
