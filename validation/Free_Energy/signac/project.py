b788f031b712cdb3b244298009737a233be30a72
go2432 <go2432@wayne.edu>
2022-10-01 22:03:55 -0500

    run the wolf/ewald prod runs for 1 day

--- validation/Free_Energy/signac/project.py
+++ validation/Free_Energy/signac/project.py
@@ -144,7 +144,7 @@ preliminary_output_avg_std_of_replicates_txt_file_name_box_0 = "preliminary_anal
 walltime_mosdef_hr = 24
 walltime_namd_hr = 24
 walltime_gomc_equilbrium_hr = 96
-walltime_gomc_production_hr = 96
+walltime_gomc_production_hr = 24
 walltime_gomc_analysis_hr = 4
 memory_needed = 16
 
