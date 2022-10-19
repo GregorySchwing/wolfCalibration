feed321f009520f51e42214037967c0ad57c5058
go2432 <go2432@wayne.edu>
2022-10-18 23:51:21 -0500

    lengthen prod

--- validation/Free_Energy/signac/project.py
+++ validation/Free_Energy/signac/project.py
@@ -147,11 +147,11 @@ preliminary_output_avg_std_of_replicates_txt_file_name_box_0 = "preliminary_anal
 walltime_mosdef_hr = 24
 walltime_namd_hr = 24
 #CPU
-walltime_gomc_equilbrium_hr = 96
+walltime_gomc_equilbrium_hr = 120
 #GPU
 #walltime_gomc_equilbrium_hr = 48
 #CPU
-walltime_gomc_production_hr = 144
+walltime_gomc_production_hr = 240
 #GPU
 #walltime_gomc_production_hr = 96
 walltime_gomc_analysis_hr = 4
