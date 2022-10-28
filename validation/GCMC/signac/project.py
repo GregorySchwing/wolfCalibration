5bd102fa820db7a0c51f3818985accf39556891b
greg <go2432@wayne.edu>
2022-10-28 15:14:59 -0500

    a

--- validation/GCMC/signac/project.py
+++ validation/GCMC/signac/project.py
@@ -1728,6 +1728,16 @@ def build_charmm(job, write_files=True):
             l = re.sub(r"%s" % "TIP3", solvent.name, l)
             sys.stdout.write(l)
 
+    with fileinput.FileInput(job.fn(f"{mosdef_structure_box_0_name_str}.psf"), inplace=True) as f:
+        for l in f:
+            l = re.sub(r"%s" % "TIP3", solvent.name, l)
+            sys.stdout.write(l)
+
+    with fileinput.FileInput(job.fn(f"{mosdef_structure_box_1_name_str}.psf"), inplace=True) as f:
+        for l in f:
+            l = re.sub(r"%s" % "TIP3", solvent.name, l)
+            sys.stdout.write(l)
+
     return [namd_charmm, gomc_charmm]
 
 
