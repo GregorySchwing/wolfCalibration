7311b61b6fe6775005f8124c136c0832ffea9b29
greg <go2432@wayne.edu>
2022-11-06 09:59:18 -0600

    A

--- validation/GEMC/signac/project.py
+++ validation/GEMC/signac/project.py
@@ -1994,7 +1994,7 @@ def build_charmm(job, write_files=True):
     #    gomc_fix_bonds_angles_residues_list  = None
     print('Running: filling liquid box')
     box_0 = mb.fill_box(compound=[solvent],
-                        density=job.doc.liquid_density,
+                        density=0.01,
                         box=[u.unyt_quantity(job.doc.liq_box_lengths_ang, 'angstrom').to_value("nm"),
                             u.unyt_quantity(job.doc.liq_box_lengths_ang, 'angstrom').to_value("nm"),
                             u.unyt_quantity(job.doc.liq_box_lengths_ang, 'angstrom').to_value("nm"),
@@ -2005,7 +2005,7 @@ def build_charmm(job, write_files=True):
 
     print('Running: filling vapor box')
     box_1 = mb.fill_box(compound=[solvent],
-                        density=job.doc.vapor_density,
+                        density=0.01,
                         box=[u.unyt_quantity(job.doc.vap_box_lengths_ang, 'angstrom').to_value("nm"),
                             u.unyt_quantity(job.doc.vap_box_lengths_ang, 'angstrom').to_value("nm"),
                             u.unyt_quantity(job.doc.vap_box_lengths_ang, 'angstrom').to_value("nm"),
@@ -2073,17 +2073,17 @@ def build_charmm(job, write_files=True):
         gomc_fix_bonds_angles=None,
     )
 
-    gomc_charmm.write_inp()
-    gomc_charmm.write_psf()
-    gomc_charmm.write_pdb()
+    #gomc_charmm.write_inp()
+    #gomc_charmm.write_psf()
+    #gomc_charmm.write_pdb()
 
-    namd_charmm.write_inp()
-    namd_charmm.write_psf()
-    namd_charmm.write_pdb()
+    #namd_charmm.write_inp()
+    #namd_charmm.write_psf()
+    #namd_charmm.write_pdb()
 
-    namd_charmm_box_1.write_inp()
-    namd_charmm_box_1.write_psf()
-    namd_charmm_box_1.write_pdb()
+    #namd_charmm_box_1.write_inp()
+    #namd_charmm_box_1.write_psf()
+    #namd_charmm_box_1.write_pdb()
 
     """
     template = get_pdb2bincoords_path()
