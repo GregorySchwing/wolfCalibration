214fafce4986c251513c6e2f475b760e8a6bbb49
greg <go2432@wayne.edu>
2022-11-07 11:52:19 -0600

    try to put legend outside box

--- validation/Free_Energy/signac/project.py
+++ validation/Free_Energy/signac/project.py
@@ -3290,8 +3290,15 @@ def part_4b_create_wolf_sanity_histograms(job):
         plt.ylabel('Probability')
         plt.xlabel('Total Energy (K)')
         plt.legend()
+        ax = plt.subplot(111)
         plt.rcParams.update({'font.size': 22})
-        plt.savefig("PotentialEnergyDistribution_Ewald_vs_{}".format(col), dpi=300)
+        # Shrink current axis by 20%
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+        # Put a legend to the right of the current axis
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        plt.savefig("PotentialEnergyDistribution_Ewald_vs_{}".format(col), dpi=300, bbox_inches='tight')
         plt.figure().clear()
   
 
