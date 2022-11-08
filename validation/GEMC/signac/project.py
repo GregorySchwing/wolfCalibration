7d374fe85ac713206477a46afdc44d49405a7c68
greg <go2432@wayne.edu>
2022-11-07 22:22:54 -0600

    5m preequil, 50 mil prod

--- validation/GEMC/signac/project.py
+++ validation/GEMC/signac/project.py
@@ -85,7 +85,7 @@ gomc_steps_lamda_production = 5 * 10**7 # set value for paper = 50 * 10**6
 gomc_console_output_data_every_X_steps = 5 * 10**2 # set value for paper = 100 * 10**3
 gomc_output_data_every_X_steps = 100 * 10**3 # set value for paper = 100 * 10**3
 #gomc_free_energy_output_data_every_X_steps = 10 * 10**3 # set value for paper = 10 * 10**3
-gomc_steps_equilb_design_ensemble = 2 * 10**7 # set value for paper = 10 * 10**6
+gomc_steps_equilb_design_ensemble = 5 * 10**6 # set value for paper = 10 * 10**6
 
 """
 During the
@@ -102,7 +102,7 @@ MC_steps = int(gomc_steps_equilb_design_ensemble)
 EqSteps = 1000
 Calibration_MC_steps = 1000000
 Calibration_MC_Eq_Steps = 10000
-Wolf_Sanity_MC_steps = 1 * 10**8
+Wolf_Sanity_MC_steps = 5 * 10**7
 
 
 # Free energy calcs: set free energy data in doc
