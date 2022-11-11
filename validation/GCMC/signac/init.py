b36a3a43fa1f4dc07b3114e7d59d7fd75096f677
greg <go2432@wayne.edu>
2022-11-10 18:15:46 -0600

    stop at 50

--- validation/GCMC/signac/init.py
+++ validation/GCMC/signac/init.py
@@ -20,19 +20,21 @@ electrostatic_method = ["Wolf", "Ewald"] # ["Ne", "Rn"]
 
 #replicas = [0, 1, 2, 3, 4] # [0, 1, 2, 3, 4]
 replicas = [0]# [0, 1, 2, 3, 4]
-shell_radius = [10, 20, 30, 40, 50, 100]* u.Angstrom# [0, 1, 2, 3, 4]
+shell_radius = [10, 20, 30, 40, 50]* u.Angstrom# [0, 1, 2, 3, 4]
+#shell_radius = [10]* u.Angstrom# [0, 1, 2, 3, 4]
 
 #production_temperatures = [275, 295, 315, 335, 355, 375] * u.K # [275, 295, 315, 335, 355, 375] * u.K
 #production_temperatures = [275] * u.K # [275, 295, 315, 335, 355, 375] * u.K
 #production_temperatures = [298] * u.K # [275, 295, 315, 335, 355, 375] * u.K
-production_temperatures = [510] * u.K # [275, 295, 315, 335, 355, 375] * u.K
+production_temperatures = [298] * u.K # [275, 295, 315, 335, 355, 375] * u.K
 
 # *******************************************
 # the main user varying state points (end)
 # *******************************************
 
-
-
+# Liq 0.9971 g/cm3 -4560
+# Liq 1.0 g/cm3 -4500
+# 298K, SPCE mu = -4540, 0.998 g/cm3
 print("os.getcwd() = " +str(os.getcwd()))
 
 pr_root = os.path.join(os.getcwd(), "src")
