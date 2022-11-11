80b13647787b90c27095d61fcba391478fa51e86
greg <go2432@wayne.edu>
2022-11-11 14:59:44 -0600

    avoid potoff33

--- validation/GEMC/signac/project.py
+++ validation/GEMC/signac/project.py
@@ -3305,7 +3305,8 @@ def run_sseq_run_gomc_command(job):
 @Project.post(part_4b_job_gomc_wolf_sanity_completed_properly)
 @Project.operation.with_directives(
     {
-        "np": lambda job: job.doc.gomc_ncpu if job.sp.electrostatic_method == "Ewald" else 8,
+        "np": 8,
+        #"np": lambda job: job.doc.gomc_ncpu if job.sp.electrostatic_method == "Ewald" else 8,
         "ngpu": lambda job: job.doc.gomc_ngpu if (job.sp.electrostatic_method == "Ewald") else 0,
         "memory": memory_needed,
         "walltime": walltime_gomc_equilbrium_hr,
