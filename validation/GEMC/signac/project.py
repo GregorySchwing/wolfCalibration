e0974fb552afb4e6b297eaa952fb7960ce09da25
greg <go2432@wayne.edu>
2022-11-30 09:05:46 -0600

    fix calcomprpo

--- validation/GEMC/signac/project.py
+++ validation/GEMC/signac/project.py
@@ -1082,13 +1082,10 @@ def part_4b_job_gomc_calibration_completed_properly(job):
         jobs = list(pr.find_jobs(ewald_sp))
         for ewald_job in jobs:
             control_file_name_str = "wolf_calibration"
-            if gomc_sim_completed_properly(
+            return gomc_sim_completed_properly(
                 ewald_job,
                 control_file_name_str,
-            ) is False:
-                return False
-            else:
-                return False
+            )
     except:
         return False
 
