cac3ed18c835f334e13e9f34300fdefbc34291c9
greg <go2432@wayne.edu>
2023-02-21 13:54:23 -0600

    use least squares since it lets e control step size

--- validation/Free_Energy/signac/src/utils/calibrator.py
+++ validation/Free_Energy/signac/src/utils/calibrator.py
@@ -6,10 +6,11 @@ import numpy as np
 import pandas as pd
 from pymbar import timeseries
 from scipy.optimize import fmin_l_bfgs_b, minimize
+from scipy.optimize import least_squares
 import matplotlib.pyplot as plt
 class Calibrator:
     def __init__(self, gomc, wolf_model, wolf_potential, target_y, initial_x, template_directory, template_control_file_name_str, \
-                 conffile, forcefield, min, max, num_iters=100):
+                 conffile, forcefield, min, max, num_iters=10):
         self.gomc = gomc
         self.wolf_model = wolf_model
         self.wolf_potential = wolf_potential
@@ -124,14 +125,14 @@ class Calibrator:
         Calibrator.copy_template(self,x)
         Calibrator.run_simulation(self)
         y_hat = Calibrator.extract_target(self, x[0])
-        self.obj_calls[x[0]] = np.abs(self.target_y-y_hat)
-        print('{0:4d}   {1: 3.6f}   {2: 3.6f}   {3: 3.6f}   {4: 3.6f}'.format(self.iteration, x[0], self.target_y, y_hat, np.abs(self.target_y-y_hat)))
+        self.obj_calls[x[0]] = 100.0*np.abs((self.target_y-y_hat)/self.target_y)
+        print('{0:4d}   {1: 3.6f}   {2: 3.6f}   {3: 3.6f}   {4: 3.6f}'.format(self.iteration, x[0], self.target_y, y_hat, 100.0*(np.abs(self.target_y-y_hat)/self.target_y)))
         with open("calibration.log", "a") as myfile:
             line = "{0: 3.6f}   {1: 3.6f}\n".format(x[0], 100.0*(np.abs(self.target_y-y_hat)/self.target_y))
             myfile.write(line)
 
         self.iteration = self.iteration + 1
-        return np.abs(self.target_y-y_hat)
+        return 100.0*np.abs((self.target_y-y_hat)/self.target_y)
     
     def calibrate(self):
         f = lambda x: Calibrator.objective(self,x)
@@ -145,8 +146,9 @@ class Calibrator:
                     iprint=99, 
                     bounds = [(self.min, self.max)]) 
         """
-        res = minimize(f, x0, method='L-BFGS-B', bounds=[(self.min, self.max)],\
-                       options={'gtol': 1e-6, 'maxiter':self.num_iters, 'iprint':101, })
+        #res = minimize(f, x0, method='L-BFGS-B', bounds=[(self.min, self.max)],\
+        #               options={'gtol': 1e-6, 'maxiter':self.num_iters, 'iprint':101, })
+        res = least_squares(f, x0, bounds=[(x0-0.1, x0+0.1)],diffstep=0.05,max_nfev=self.num_iters)
         self.x = res.x
         self.fun = res.fun
         self.converged = res.success
